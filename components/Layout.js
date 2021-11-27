baa340688607a1be9b4628c907c20606739fe1b0
Emmanuel Adegor <emmanueladegor@Emmanuels-MacBook-Pro.local>
2021-11-27 17:55:12 +0100

    initial commit

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,7 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
+              <span>RECIPE APP</span>
             </h1>
             <h2>Spread The Joy</h2>
           </a>
@@ -20,7 +19,7 @@ export default function Layout({ children }) {
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>&copy; 2021 RECIPE</p>
       </footer>
     </div>
   )
